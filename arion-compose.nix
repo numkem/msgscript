bcdae86a4c128b02448e218716cf842f97566b88
Sebastien Bariteau <numkem@numkem.org>
2024-09-28 23:46:50 -0400

    fix etcd listening to the actual port

--- arion-compose.nix
+++ arion-compose.nix
@@ -7,7 +7,13 @@
     etcd = {
       image.enableRecommendedContents = true;
       service.useHostStore = true;
-      service.command = [ "${pkgs.etcd_3_5}/bin/etcd" ];
+      service.command = [
+        "${pkgs.etcd_3_5}/bin/etcd"
+        "-advertise-client-urls"
+        "http://127.1.1.1:2379"
+        "-listen-client-urls"
+        "http://0.0.0.0:2379"
+      ];
       service.ports = [
         "2379:2379"
       ];
