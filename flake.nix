d7899f52edee7670a77a0e1e4aba702881445514
Sebastien Bariteau <numkem@numkem.org>
2025-10-24 21:50:21 -0400

    update nix version

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs =
     { self, nixpkgs }:
     let
-      version = "0.7.1";
+      version = "0.8.1";
       vendorHash = "sha256-IsLAIKYuKhlD71fad8FuayTFbdQJla4ifjs8TexXDYQ=";
 
       mkPlugin =
